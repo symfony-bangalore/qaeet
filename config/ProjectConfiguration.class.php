ba288dbd2159bfd3cc82855cdd6a10ba5e128328
Sjoerd de Jong <sjoerd@weett.nl>
2010-02-13 11:17:33 +0530

    initial commit

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'C:\\weett\\lib\\symfony\\1.4\\lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
